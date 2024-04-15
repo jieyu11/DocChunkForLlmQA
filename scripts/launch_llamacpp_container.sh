8d5bc04c8c6841bb053d357716e87e357083d078
Jie Yu <universe_bright@jies-mac-mini.mynetworksettings.com>
2024-04-15 14:01:44 -0400

    minor update shell script

--- scripts/launch_llamacpp_container.sh
+++ scripts/launch_llamacpp_container.sh
@@ -6,7 +6,10 @@
 models_folder=${HOME}/Workarea/Projects/references/models/TheBloke
 model_file=llama-2-7b-chat.Q2_K.gguf
 
-docker run -p 8080:8080 -v ${models_folder}:/models \
+container_name=llamacpp_server
+docker run -dt \
+    --name ${container_name} \
+    -p 8080:8080 -v ${models_folder}:/models \
     --network my-network \
     ghcr.io/ggerganov/llama.cpp:server -m /models/${model_file} \
     -c 512 --host 0.0.0.0 --port 8080
--- scripts/test_chatbot.sh
+++ scripts/test_chatbot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Note: vigilant_cray is the running container from llama.cpp image
-# 
-docker exec -it myunstructured bash -c "python3 src/chatbot.py -u http://vigilant_cray:8080/completion -i input/adobe_pdfs/2256935.pdf -q \"What was the gross profit of three months ended in 2013?\"" 
+# Note: running container name from llama.cpp image is needed
+# `"..."` is needed to break the long string
+llamacpp_container_name=llamacpp_server
+docker exec -it myunstructured bash -c \
+    "python3 src/chatbot.py -u http://${llamacpp_container_name}:8080/completion "`
+    `"-i input/adobe_pdfs/2256935.pdf -q \"What was the gross "`
+    `"profit of three months ended in 2013?\"" 
